34f3031877ce40c977e3af1757281ae0ee57a86a
AyshaAfnaan <ayshaafnaan23@gmail.com>
2023-11-16 21:14:15 +0530

    updated title

--- scriipt.js
+++ scriipt.js
@@ -1,21 +1,3 @@
-let className = "white";
-let scrollTrigger = 550;
-let buttonClassName = "btn-green";
-
-window.onscroll = function () {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    document.getElementById("nav").classList.add(className);
-    document.getElementById("start").classList.add(buttonClassName);
-  } else {
-    document.getElementById("nav").classList.remove(className);
-    document.getElementById("start").classList.remove(buttonClassName);
-  }
-};
-
-
-
-
 function mAnimation() {
     let toggle = document.querySelectorAll("svg > g > g > g");
     let random = Math.floor(Math.random() * toggle.length);
